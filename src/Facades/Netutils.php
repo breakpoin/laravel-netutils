92b8cf8c11a458b36e78f787a7a11c3fa71a7cc4
Fahmi L. Ramdhani <fahmilestianramdhani@gmail.com>
2019-04-02 17:04:03 +0700

    Initial commit

---
+++ src/Facades/Netutils.php
@@ -0,0 +1,10 @@
+<?php
+namespace Breakpoin\Netutils\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Netutils extends Facade {
+
+    protected static function getFacadeAccessor() { return 'netutils'; }
+
+}
